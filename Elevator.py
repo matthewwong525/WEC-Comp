ec59dfcc74f43b5e5f0c1907f5d7a17714954e0c
kevin-f-liu <kevin.f.liu2012@gmail.com>
2017-05-12 22:31:00 -0400

    Add files via upload

--- Elevator.py
+++ Elevator.py
@@ -12,24 +12,12 @@ class Elevator:
         self.floors = []
         # Current data
         self.current_floor = 0
-        self.num_people = 0
         # Directional Data (-1 = down, 0 = stop, 1 = up)
         self.direction = 0
         # State
-        self.state = "stopped"
-
-    def update(self, floors):
-        floors_with_people = []
-        State = Enum('State', 'stopped moving stopping')
-        # Search for floors with people in the same direction
-        for floor in floors:
-            if floor.numPeople > 0:
-                if self.direction > 0 and floor.floorNum >= self.current_floor:
-                    floors_with_people.append(floor.floorNum)
-                elif self.direction < 0 and floor.floorNum <= self.current_floor:
-                    floors_with_people.append(floor.floorNum)
-        for
-
+        self.state = 'stopped'
+        self.mustWait = 0
+        self.waitTime = 0
 
     def addPerson(self, person):
         if len(self.people) == self.max_people:
@@ -42,3 +30,40 @@ class Elevator:
         for person in self.people:
             if person.dest == self.current_floor:
                 self.people.remove(person)
+
+    def update(self, floors):
+        floors_with_people = []
+
+        # Search for floors with people in the same direction
+        for floor in floors:
+            if floor.numPeople > 0:
+                if self.direction > 0 and floor.floorNum >= self.current_floor:
+                    floors_with_people.append(floor.floorNum)
+                elif self.direction < 0 and floor.floorNum <= self.current_floor:
+                    floors_with_people.append(floor.floorNum)
+                elif self.direction == 0:
+                    floors_with_people.append(floor.floorNum)
+        if len(floors_with_people) == 0 and len(self.people) == 0:
+            self.state = 'stopped'
+
+        if self.state == 'stopped':
+            if floors_with_people[0] > self.current_floor:
+                self.direction = 1;
+            elif floors_with_people < self.current_floor:
+                self.direction = -1;
+
+
+        if (self.state == 'moving' or self.state == 'stopping') and self.waitTime < self.mustWait:
+            # Dont do anything if the elevator is in motion
+            self.waitTime += 1
+            return
+        # If the elevator is moving and it has moved for 3 ticks
+        if self.state == 'moving' and self.waitTime == self.mustWait:
+            self.current_floor += self.mustWait * self.direction / 3
+
+        for person in self.people:
+
+        if self.direction == 0:  # Stopped
+            for floor in floors_with_people:
+                # Get the next floor
+                self.arrive()
