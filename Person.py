fe5c6258e66203c400e43166547878c8cd6b703f
Janakan Skiwalker <janakan.chandru@gmail.com>
2017-05-12 21:30:46 -0400

    added person class

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, appearTime, totalTime, curr_floor, go_floor):
+        self.appearTime = 0
+        self.totalTime = 0
+        self.go_floor = 0
+        self.curr_floor = 0
+
+    def get_waitTime(self, time): 
+        time = totalTime - appearTime
+        return time
+
+    
